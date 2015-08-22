c325fb0c8b83fe6a394d224b148dc415c3368cbc
reissm <mattreiss@Matt-Reisss-MacBook-Pro.local>
2015-08-21 20:27:25 -0400

    New .js files, pulling data
    Added graphing capabilities for angular and js. Pulling and storing
    item data in JSON format. Removed API key for security purposes.

--- WebContent/app/js/main.js
+++ WebContent/app/js/main.js
@@ -1,4 +1,4 @@
-angular.module('main', ['ui.bootstrap'])
+angular.module('main', ['ui.bootstrap', 'chart.js'])
 .config(['$httpProvider', function ($httpProvider) {
 	$httpProvider.defaults.useXDomain = true;
     delete $httpProvider.defaults.headers.common['X-Requested-With'];
@@ -8,29 +8,43 @@ angular.module('main', ['ui.bootstrap'])
 	$scope.matchList = {};
 	$scope.curMatch = "";
 	$scope.gameType = "NORMAL_5X5";
-	$scope.items = {};
+	$scope.items511 = {};
+	$scope.items514 = {};
+	
+	$scope.labels =["Ranked", "Normals", "Sleeping"];
+
+	  $scope.data = [
+	    [65, 59, 54],
+	    [28, 48, 22]
+	  ];
 	
 	$scope.getUserIPInfo = function(){
-		mainService.pullData($scope.region, $scope.curMatch, $scope.gameType).then(function(data){
-			if(data.data.message == 'invalid query'){
-				$scope.ipinfo = {message: data.data.message};
-			} else {
-				$scope.ipinfo = data.data.participants;
-				console.log($scope.ipinfo);
-				$scope.items = mainService.pullItemInfo($scope.ipinfo);
-			}
-		});
+		
+		for(var i = 0; i < 5; i++) {
+			$scope.curMatch = pickRandomMatch($scope.matchlist);
+			
+			mainService.pullData($scope.region, $scope.curMatch).then(function(data){
+				if(data.data.message == 'invalid query'){
+					$scope.ipinfo = {message: data.data.message};
+				} else {
+					$scope.ipinfo = data.data.participants;
+					
+					mainService.pullItemInfo($scope.ipinfo,$scope.items511)
+				}
+			});
+		}
+
+		console.log($scope.items511);
 	};
 	
 	$scope.setRegion = function(newR) {
 		$scope.region = newR;
 		
-		mainService.getMatches($scope.region).then(function(data){
+		mainService.getMatches($scope.region, $scope.gameType).then(function(data){
 			if(data.data.message == 'invalid query'){
 				$scope.matchlist = {message: data.data.message};
 			} else {
 				$scope.matchlist = data.data;
-				$scope.curMatch = pickRandomMatch($scope.matchlist);
 			}
 		});
 	};
@@ -43,8 +57,8 @@ angular.module('main', ['ui.bootstrap'])
 		pullItemInfo: pullItemInfo
 	};
 	
-	function pullData(region, curMatch, gameType){
-		var apiKey = "4e635d25-7bde-453f-bcd3-8777ef544e7b";
+	function pullData(region, curMatch){
+		var apiKey = "";
 		
 		if(region == ("Select Region" || null)){
 			region = "na";
@@ -67,11 +81,13 @@ angular.module('main', ['ui.bootstrap'])
 		});
 	}
 	
-	function getMatches(region) {
+	function getMatches(region, gameType) {
 		if(region == ("Select Region" || null)){
 			region = "NA";
 		}
-		var url = 'app/assets/AP_ITEM_DATASET/5.11/RANKED_SOLO/' + region + ".json";
+		
+		var url = 'app/assets/AP_ITEM_DATASET/5.11/GAMETYPE/REGION.json';
+		url = url.replace('GAMETYPE', gameType).replace('REGION', region)
 		
 		var request = $http({
 			method: 'GET',
@@ -86,11 +102,14 @@ angular.module('main', ['ui.bootstrap'])
 		});
 	}
 	
-	function pullItemInfo(players){
-		var items = {};
+	function pullItemInfo(players, items){
 		for(var i = 0; i < players.length; i++){
 			for(var j = 0; j < 6; j++){
-				items["item"+((i*6)+j)] = players[i].stats["item"+j]
+				if((items[players[i].stats["item"+j]] == undefined)){
+					items[players[i].stats["item"+j]] = 1;
+				} else {
+					items[players[i].stats["item"+j]] = items[players[i].stats["item"+j]] + 1;
+				}
 			}
 		}
 		console.log(items);
